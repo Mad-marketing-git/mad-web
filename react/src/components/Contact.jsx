4f5e1b586b7a53a9e467911e9d750550eba4e3cc
Theekshana <Theekshana@DESKTOP-QT7JB5G>
2025-08-15 14:19:25 +0530

    feat: Fine-tune Page 6 (Contact) design and animations

--- react/src/components/Contact.jsx
+++ react/src/components/Contact.jsx
@@ -1,17 +1,18 @@
 
 import React from 'react';
 import styles from './Contact.module.css';
-import skyline from '../assets/IMAGE ASSETS BG REMOVED/TB SKYLINE BG.png';
-import road from '../assets/IMAGE ASSETS BG REMOVED/TB ROAD.png';
+import background from '../assets/IMAGE ASSETS BG REMOVED/page-6/page-6-bg.png';
 import redCar from '../assets/IMAGE ASSETS BG REMOVED/TB RED CAR.png';
 import greenCar from '../assets/IMAGE ASSETS BG REMOVED/TB GREEN CAR.png';
 import pinkCar from '../assets/IMAGE ASSETS BG REMOVED/TB PINK CAR.png';
 import yellowCar from '../assets/IMAGE ASSETS BG REMOVED/TB YELLOW CAR.png';
 import yellowBus from '../assets/IMAGE ASSETS BG REMOVED/TB YELLOW BUS.png';
-
+import wheel from '../assets/3wheel.png';
+import van from '../assets/van.png';
 const Contact = () => {
   return (
     <div className={styles.page6Container}>
+      <img src={background} alt="background" className={styles.backgroundImage} />
       <div className={styles.content}>
         <div className={styles.talkToUs}>
           <h2>TALK TO US</h2>
@@ -25,13 +26,13 @@ const Contact = () => {
         </div>
       </div>
       <div className={styles.footerCityscape}>
-        <img src={skyline} alt="skyline" className={styles.skyline} />
-        <img src={road} alt="road" className={styles.road} />
         <img src={redCar} alt="red car" className={`${styles.car} ${styles.redCar}`} />
         <img src={greenCar} alt="green car" className={`${styles.car} ${styles.greenCar}`} />
         <img src={pinkCar} alt="pink car" className={`${styles.car} ${styles.pinkCar}`} />
         <img src={yellowCar} alt="yellow car" className={`${styles.car} ${styles.yellowCar}`} />
-        <img src={yellowBus} alt="yellow bus" className={`${styles.car} ${styles.yellowBus}`} />
+        <img src={yellowBus} alt="yellow bus" className={`${styles.bus} ${styles.yellowBus}`} />
+        <img src={wheel} alt="3 wheel" className={`${styles.wheel} ${styles.threeWheel}`} />
+        <img src={van} alt="van" className={`${styles.van} ${styles.whiteVan}`} />
       </div>
     </div>
   );
